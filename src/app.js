75af9e458e6a3b80729462270ee04d894ce180df
Kayyoom Ali <techali1718@gmail.com>
2025-07-10 23:01:02 +0530

    feat: getUserFromEmail, getFeed, signup api

--- src/app.js
+++ src/app.js
@@ -3,19 +3,52 @@ const express = require("express");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "kayyoom",
-    lastName: "Ali",
-    email: "kayyoom@gmail.com",
-    password: "kayyoom@131",
-  };
+  console.log(req.body);
+
   try {
-    const user = new User(userObj);
+    const user = new User(req.body);
     await user.save();
     res.send("User added successfully to Db");
   } catch (error) {
     res.status(400).send("Error saving the use" + error.message);
+  }
+});
+
+//get the user from email
+app.get("/getUserFromEmail", async (req, res) => {
+  const userEmail = req.body.email;
+
+  //try multiple method to find user , by id or etc
+
+  // findOne method returns first matched id and its object not array
+  // try {
+  //   const userFound = await User.findOne({ email: userEmail });
+  //   if (!userFound) res.status(404).send("user not found with email" + userEmail);
+  //   res.send(userFound);
+  // } catch (error) {
+  //   res.status(400).send("something went wrong");
+  // }
+
+  try {
+    const userFound = await User.find({ email: userEmail });
+    if (userFound.length === 0) res.status(404).send("user not found with email" + userEmail);
+    res.send(userFound);
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//get the feed to app
+app.get("/getFeed", async (req, res) => {
+  try {
+    const userFound = await User.find({});
+    if (userFound.length === 0) res.status(404).send("users not found");
+    res.send(userFound);
+  } catch (error) {
+    res.status(400).send("something went wrong");
   }
 });
 
